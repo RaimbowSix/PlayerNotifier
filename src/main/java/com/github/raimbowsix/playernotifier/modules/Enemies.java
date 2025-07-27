8e1d8659f776fd5d3a8ed2905dce2ef7b56fd0a4
RaimbowSix <86539713+RaimbowSix@users.noreply.github.com>
2025-07-27 20:14:41 +0200

    Update Enemies.java

--- src/main/java/com/github/raimbowsix/playernotifier/modules/Enemies.java
+++ src/main/java/com/github/raimbowsix/playernotifier/modules/Enemies.java
@@ -1,11 +1,11 @@
  package com.github.raimbowsix.playernotifier.modules;
 
 import com.github.raimbowsix.playernotifier.PlayerNotifier;
+import com.github.raimbowsix.playernotifier.config.ConfigOneConfig;
 import com.github.raimbowsix.playernotifier.util.WatchlistManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetworkPlayerInfo;
-import net.minecraft.util.BlockPos;
-
+import net.minecraft.entity.player.EntityPlayer;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,7 +13,9 @@ public class Enemies {
 
     private static final Minecraft mc = Minecraft.getMinecraft();
     public static Set<String> lastEnemySet = new HashSet<>();
-
+    public static boolean isEnemy (EntityPlayer player){
+        return WatchlistManager.isEnemy(player.getName());
+    }
     public static void notifyIfEnemiesInLobby() {
         Set<String> currentEnemySet = new HashSet<>();
         for (NetworkPlayerInfo info : PlayerNotifier.players) {
@@ -22,16 +24,19 @@ public class Enemies {
                 currentEnemySet.add(name.toLowerCase());
             }
         }
-        for (String name : currentEnemySet) {
+        if (ConfigOneConfig.enemyChatNotification){
+            for (String name : currentEnemySet) {
             if (!lastEnemySet.contains(name)) {
                 PlayerNotifier.sendMessage("§8[§a+§8] §7[§6PlayerNotifier§7] §4Enemy §8» §c" + name + " §fhas joined the lobby!");
             }
         }
-        for (String name : lastEnemySet) {
-            if (!currentEnemySet.contains(name)) {
-                PlayerNotifier.sendMessage("§8[§c-§8] §7[§6PlayerNotifier§7] §4Enemy §8» §c" + name + " §fhas left the lobby!");
+            for (String name : lastEnemySet) {
+                if (!currentEnemySet.contains(name)) {
+                    PlayerNotifier.sendMessage("§8[§c-§8] §7[§6PlayerNotifier§7] §4Enemy §8» §c" + name + " §fhas left the lobby!");
+                }
             }
         }
+
         lastEnemySet.clear();
         lastEnemySet.addAll(currentEnemySet);
     }
