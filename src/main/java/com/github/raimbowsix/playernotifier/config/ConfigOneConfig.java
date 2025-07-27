c82e7a7f71976d8643556532feef9c2181b8ccf5
RaimbowSix <86539713+RaimbowSix@users.noreply.github.com>
2025-07-27 19:59:53 +0200

    Update ConfigOneConfig.java

--- src/main/java/com/github/raimbowsix/playernotifier/config/ConfigOneConfig.java
+++ src/main/java/com/github/raimbowsix/playernotifier/config/ConfigOneConfig.java
@@ -12,24 +12,24 @@ import cc.polyfrost.oneconfig.config.data.Mod;
 import cc.polyfrost.oneconfig.config.data.ModType;
 import cc.polyfrost.oneconfig.config.data.OptionSize;
 
-import javax.swing.text.html.Option;
-
 
 public class ConfigOneConfig extends Config {
     //categories
     private static final transient String ENEMIES = "Enemies";
     private static final transient String DARK = "Darks";
     private static final transient String DENICKER = "Denicker";
-    //subcategories
+
     private static final transient String ENEMIESOPTIONAL = "Optional";
     private static final transient String DARKOPTIONAL = "Optional";
     private static final transient String DENICKEROPTIONAL = "Optional";
 
     //Dark Pants
+
     @HUD(
             name = "Dark Pants HUD",
             category = DARK
     )
+
     public static DarkHud darkHud = new DarkHud();
     @Switch(
             name = "Display Distance",
@@ -39,7 +39,7 @@ public class ConfigOneConfig extends Config {
     )
     public static boolean darkDistance = true;
     @Switch(
-            name = "Display Dark Position",
+            name = "Display Position",
             size = OptionSize.SINGLE,
             category = DARK,
             subcategory = DARKOPTIONAL
@@ -47,11 +47,13 @@ public class ConfigOneConfig extends Config {
     public static boolean darkPosition = true;
 
     //Nicked Players
+
     @HUD(
             name = "Nicked Players HUD",
             category = DENICKER
     )
     public static NickedHud nickedHud = new NickedHud();
+
     @Switch(
             name = "Display Distance",
             size = OptionSize.SINGLE,
@@ -59,14 +61,29 @@ public class ConfigOneConfig extends Config {
             subcategory = DENICKEROPTIONAL
     )
     public static boolean nickedDistance = true;
+    @Switch(
+            name = "Display Position",
+            size = OptionSize.SINGLE,
+            category = DENICKER,
+            subcategory = DENICKEROPTIONAL
+    )
+    public static boolean nickedPosition = true;
+    @Switch(
+            name = "Chat Notification",
+            description = "sends chat message when a nicked player joins",
+            size = OptionSize.SINGLE,
+            category = DENICKER,
+            subcategory = DENICKEROPTIONAL
+    )
+    public static boolean nickedChatNotification = true;
 
     //Enemy Players
     @HUD(
             name = "Enemies HUD",
             category = ENEMIES
     )
-
     public static EnemiesHud enemiesHud = new EnemiesHud();
+
     @Switch(
             name = "Display Distance",
             size = OptionSize.SINGLE,
@@ -74,29 +91,21 @@ public class ConfigOneConfig extends Config {
             subcategory = ENEMIESOPTIONAL
     )
     public static boolean enemyDistance = true;
-
-
-
-
-    //examples from example mod
     @Switch(
-            name = "Example Switch",
-            size = OptionSize.SINGLE // Optional
+            name = "Display Position",
+            size = OptionSize.SINGLE,
+            category = ENEMIES,
+            subcategory = ENEMIESOPTIONAL
     )
-    public static boolean exampleSwitch = false; // The default value for the boolean Switch.
-
-    @Slider(
-            name = "Example Slider",
-            min = 0f, max = 100f, // Minimum and maximum values for the slider.
-            step = 10 // The amount of steps that the slider should have.
+    public static boolean enemyPosition = true;
+    @Switch(
+            name = "Chat Notification",
+            description = "sends chat message when an enemy player joins",
+            size = OptionSize.SINGLE,
+            category = ENEMIES,
+            subcategory = ENEMIESOPTIONAL
     )
-    public static float exampleSlider = 50f; // The default value for the float Slider.
-
-    @Dropdown(
-            name = "Example Dropdown", // Name of the Dropdown
-            options = {"Option 1", "Option 2", "Option 3", "Option 4"} // Options available.
-    )
-    public static int exampleDropdown = 1; // Default option (in this case "Option 2")
+    public static boolean enemyChatNotification = true;
 
     public ConfigOneConfig() {
         super(new Mod(PlayerNotifier.NAME, ModType.UTIL_QOL), PlayerNotifier.MODID + ".json");
