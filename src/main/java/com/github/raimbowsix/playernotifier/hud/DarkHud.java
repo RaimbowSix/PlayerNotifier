f260fcd2595edb673e11cfae589a6ecb25e2e6b2
RaimbowSix <86539713+RaimbowSix@users.noreply.github.com>
2025-07-29 23:56:51 +0200

    Update DarkHud.java

--- src/main/java/com/github/raimbowsix/playernotifier/hud/DarkHud.java
+++ src/main/java/com/github/raimbowsix/playernotifier/hud/DarkHud.java
@@ -22,12 +22,7 @@ public class DarkHud extends TextHud {
                 Minecraft.getMinecraft().theWorld.playerEntities.stream()
                     .filter(DarkPants::hasDarks)
                     .forEach(p->{
-<<<<<<< HEAD
-                        Darks.add(p.getDisplayName().getFormattedText()+ PlayerLocation.getPlayerDistance(p.getName(), ConfigOneConfig.darkDistance));
-=======
-                        Darks.add(p.getDisplayName().getFormattedText()+" "+GetEnchants.getDarkPantsEnchantFromName(p)+
-                            PlayerLocation.getPlayerDistance(p.getName(), ConfigOneConfig.darkDistance));
->>>>>>> 9894e61267afd7773410df65ad4302406cfe45e7
+                        Darks.add(p.getDisplayName().getFormattedText()+GetEnchants.getDarkPantsEnchantFromName(p)+PlayerLocation.getPlayerDistance(p.getName(), ConfigOneConfig.darkDistance));
                 });
             lines.add("§5Darks: " + Darks.size());
             lines.addAll(Darks);
