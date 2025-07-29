440689639d12d8844f533a4104e07b170332354b
RaimbowSix <86539713+RaimbowSix@users.noreply.github.com>
2025-07-29 23:58:39 +0200

    Update DarkPants.java

--- src/main/java/com/github/raimbowsix/playernotifier/modules/DarkPants.java
+++ src/main/java/com/github/raimbowsix/playernotifier/modules/DarkPants.java
@@ -1,20 +1,10 @@
 package com.github.raimbowsix.playernotifier.modules;
 
 import com.github.raimbowsix.playernotifier.PlayerNotifier;
-<<<<<<< HEAD
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
-=======
-import com.github.raimbowsix.playernotifier.util.EPitEnchants;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.network.NetworkPlayerInfo;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.common.util.Constants;
->>>>>>> 9894e61267afd7773410df65ad4302406cfe45e7
 
 import java.util.HashSet;
 import java.util.Set;
@@ -32,15 +22,9 @@ public class DarkPants {
 
 
     public static Set<String> lastDarkSet = new HashSet<>();
-<<<<<<< HEAD
     public static void detectIfPlayerHasDarkPants() {
         Set<String> currentDarkSet = new HashSet<>();
         for (EntityPlayer player : PlayerNotifier.playerEntities) {
-=======
-    public static void notifyIfPlayerHasDarkPants() {
-        Set<String> currentDarkSet = new HashSet<>();
-        for (EntityPlayer player : Minecraft.getMinecraft().theWorld.playerEntities) {
->>>>>>> 9894e61267afd7773410df65ad4302406cfe45e7
             String name = player.getName();
             if (hasDarks(player)) {
                 currentDarkSet.add(name.toLowerCase());
